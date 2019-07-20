6a5d7c26cf5e535c5fa78fe3347ebcc2e8d8dd76
thankcreate <thankcreate@gmail.com>
2019-07-20 00:43:33 -0400

    removed redundant log

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as webview from "./webview";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "power-fsm-viewer" is now active!');
@@ -42,14 +42,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 		vscode.window.onDidChangeTextEditorSelection(
 			e => {
-				
+
 				if (e.textEditor === vscode.window.activeTextEditor) {
 					panel.webview.html = webview.getWebviewContent(context);
-					console.log('haha');
-				  }
-				  else {
-					  console.log("hehe");
-				  }
+				}
 			},
 			null,
 			disposables
