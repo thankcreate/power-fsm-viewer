2d1fea1be42df955c6d084b5f5989a4f0b1aa62a
thankcreate <thankcreate@gmail.com>
2019-07-19 22:56:54 -0400

    callback added, config added

--- src/webview.ts
+++ src/webview.ts
@@ -7,12 +7,18 @@ import * as parser from '@babel/parser'
 import traverse from "@babel/traverse";
 
 const generator = require('@babel/generator').default;
+type INode = babel.types.Node;
+type IPath = babel.NodePath<babel.types.Node>
 
-
+interface CallbackInfo {
+    key: string,
+    value: string
+}
 
 interface ParseResult {
-    fsmNames: string[],
-    content: string,
+    fsmVarName: string,
+    code: string,
+    callbacks: CallbackInfo[];
 }
 
 export function getWebviewContent(context: vscode.ExtensionContext) {
@@ -25,17 +31,16 @@ export function getWebviewContent(context: vscode.ExtensionContext) {
         return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
     });
 
-    var parseResult; 
-    var config;
+    let parseResult;
+    let config;
     try {
-        var code = getCode();
-        parseResult = getFsmContent(code, context);    
-        config = getConfig();
+        parseResult = getFsmContent(context);
+        config = getConfig();        
     } catch (error) {
         return error.message;
     }
-    
-    if(!config)
+
+    if (!config)
         config = {};
 
     var renderData = {
@@ -49,8 +54,10 @@ function getWrappedFsmContent(parseResult: ParseResult, config: any) {
     let block = `
     <script>       
         let targetFsm;         
-        ${parseResult.content}
-        targetFsm = ${parseResult.fsmNames[0]};
+        ${parseResult.code}
+        targetFsm = ${parseResult.fsmVarName};
+
+        let callbacks = ${JSON.stringify(parseResult.callbacks)};        
 
         let config;
         config = ${JSON.stringify(config)};
@@ -59,7 +66,7 @@ function getWrappedFsmContent(parseResult: ParseResult, config: any) {
     return block;
 }
 
-function getConfig() { 
+function getConfig() {
     let activeEditor = vscode.window.activeTextEditor;
 
     if (!activeEditor)
@@ -67,17 +74,40 @@ function getConfig() {
 
     let activeContent = activeEditor.document.getText();
 
-    let reg = /(^\s*(\/\*|[\/]{2})\s*viz-fsm-viewer-config\s*:)(.*)$/m
-    let find = activeContent.match(reg);
-    if(!find)
+    let reg = /(^\s*(\/\*|[\/]{2,}).*fsm-config\s*:)(.*)$/mg
+
+    let matches, output = [];
+    while (matches = reg.exec(activeContent)) {
+        output.push(matches[3]);
+    }
+
+    let obArr = [];
+
+    try {
+        for(let i in output) {
+            let ob = JSON.parse(output[i]);
+            obArr.push(ob);
+        }
+    }
+    catch (error) {
+        throw new Error('Config parse failed');        
+    }
+
+    if(obArr.length === 0)
         return null;
-    
-    let jsonConfig = find[3];    
-    let obj = JSON.parse(jsonConfig);
-    return obj;
+
+    // merge all config into one
+    let config:any = {};
+    obArr.forEach(e=>{
+        for(let i in e) {
+            config[i] = e[i];
+        }
+    })
+
+    return config;
 }
 
-function getCode() {
+function getClampedCode(): string | undefined {
     let activeEditor = vscode.window.activeTextEditor;
 
     if (!activeEditor)
@@ -85,50 +115,154 @@ function getCode() {
 
     let activeContent = activeEditor.document.getText();
 
-    let reg = /viz-fsm-viewer-begin.*?$([\s\S]*)(\/\*|[\/]{2})\s*viz-fsm-viewer-end/m;
+    let reg = /fsm-begin.*?$([\s\S]*)^\s*(\/\*|[\/]{2}).*fsm-end/m;
     let find = activeContent.match(reg);
-    if(find && find[1]) {
+    if (find && find[1]) {
         return find[1];
     }
 
-    return activeContent;
+    return undefined;
 }
 
 
-function getFsmContent(code: string, context: vscode.ExtensionContext) : ParseResult {
+function getFsmContent(context: vscode.ExtensionContext): ParseResult {
+    let activeEditor = vscode.window.activeTextEditor;
+
+    if (!activeEditor)
+        throw new Error("No editor opened >_<");
+
+    let code = activeEditor.document.getText();
+
+    // If have user forced clamped code, use the scope to AST
+    let clampedCode = getClampedCode();
+    if (clampedCode)
+        code = clampedCode;
+
+    // Babel!
     let astResult = babel.transformSync(code, {
         code: false,
         ast: true,
-        cwd: context.extensionPath,      
-        plugins: [            
+        cwd: context.extensionPath,
+        plugins: [
             '@babel/plugin-proposal-class-properties',
             "@babel/plugin-transform-typescript"
         ]
     });
     if (!astResult)
         throw new Error("FSM not found >_<");
-        
 
-    let ast = astResult.ast; 
+    let ast = astResult.ast;
 
-    // fsms
-    let fsmRes = getFsms(ast, code, context);
-
-    // combined 
-    // let combinedCode = varsContent + '\n'+ fsmRes.content;
-    let combinedCode = generator(ast).code;
-    
-    if(fsmRes.content.trim() === '')
+    // Get all FSM nodes
+    let fsmNodes = getFsmNodes(ast, code, context);
+    if (!fsmNodes || fsmNodes.length == 0)
         throw new Error("FSM not found >_<");
 
-    let ret:ParseResult = {
-        fsmNames: fsmRes.fsmNames, 
-        content: combinedCode
-    };
+    // Choose target FSM
+    let targetFsmPath = fsmNodes[0];
+
+    // Handle the given FSM
+    let fsmResult = parseFsm(targetFsmPath);
+
+    // If user define the clamped code zone, use that zone    
+    let generatedCode = '';
+    if (clampedCode) {
+        generatedCode = generator(ast).code;
+    }
+    // otherwise only generate the FSM VariableDeclaration code
+    else {
+        generatedCode = generator(targetFsmPath.node).code;
+    }
+
+    let ret = fsmResult;
+    ret.code = generatedCode;
 
     return ret;
 }
 
+function parseFsm(fsmPath: IPath): ParseResult {
+    let res: ParseResult = { fsmVarName: "", code: "", callbacks: []};
+
+    res.fsmVarName = (<any>fsmPath.node).declarations[0].id.name;
+    // parse and remove callback nodes
+    babel.traverse(fsmPath.node, {
+        enter: path => {
+            const { node, parent } = path;
+            if (isCallback(node, path)) {
+                let callbacks = parseCallbacks(path);
+                res.callbacks = callbacks;
+                path.remove();
+            }
+        }
+    }, fsmPath.scope, undefined, fsmPath);
+
+    // remove condition nodes
+    babel.traverse(fsmPath.node, {
+        enter: path => {
+            const { node, parent } = path;
+            if (isCondition(node, path)) {
+                path.remove();
+            }
+        }
+    }, fsmPath.scope, undefined, fsmPath);
+
+    res.code = generator(fsmPath.node).code;
+    return res;
+}
+
+function parseCallbacks(path: IPath): CallbackInfo[] { 
+    let ret:CallbackInfo[] =  [];
+    let node:any = path.node;
+    
+    if(!node || !node.value || !node.value.properties || node.value.properties.length === 0)
+        return ret;
+
+    let properties = node.value.properties;
+    for(let i in properties) {
+        let prop = properties[i];
+        let singleRes = parseSingleCallback(prop);
+        if(singleRes) {
+            ret.push(singleRes);
+        }
+    }
+
+    return ret;
+}
+
+function parseSingleCallback(node: any) : CallbackInfo | undefined {
+    let ret: CallbackInfo | undefined = undefined;
+
+    try {
+        if (!node || !node.value || !node.key || !node.key.name)
+            return undefined;
+
+        let keyName = node.key.name;
+        // onState: ()=>{}
+        if (node.value.type === 'ArrowFunctionExpression') {
+            ret = { key: keyName, value: keyName };
+        }
+        else if (node.value.type === 'FunctionExpression') {
+            // onState: function func(){}
+            if (node.value.id && node.value.id.name) {
+                ret = { key: keyName, value: node.value.id.name };
+            }
+            // onState: function (){}
+            else {
+                ret = { key: keyName, value: keyName };
+            }
+        }
+        // onState: varFunc;
+        if (node.value.type === 'Identifier') {
+            if (node.value.name)
+                ret = { key: keyName, value: node.value.name };
+        }
+    } 
+    catch (error) {
+        console.log("Single callback parse failed");
+    }
+
+    return ret;
+}
 
 
 function isFsmVariableDeclarator(node: any): boolean {
@@ -157,7 +291,6 @@ function isFsmVariableDeclarator(node: any): boolean {
     return false;
 };
 
-
 function isFsmVariableDeclaration(node: any): boolean {
     return (
         node.type === 'VariableDeclaration'
@@ -166,20 +299,34 @@ function isFsmVariableDeclaration(node: any): boolean {
     )
 }
 
-function getFsms(ast: any, code: string, context: vscode.ExtensionContext) : ParseResult{
-    let targets: any[] = [];
-    let names: string[] = [];
+function isCallback(node: any, path: IPath): boolean {
+    return (path.isObjectProperty && node && node.key && node.key.name === 'callbacks');
+}
+
+function isCondition(node: any, path: IPath): boolean {
+    return (path.isProperty && node && node.key && node.key.name === 'condition');
+}
+
+function getFsmNodes(ast: any, code: string, context: vscode.ExtensionContext): IPath[] {
+    let targets: IPath[] = [];
+
     babel.traverse(ast, {
-        enter: (path:any) => {
+        enter: path => {
             const { node, parent } = path;
             if (isFsmVariableDeclaration(node)) {
-                targets.push(node);
-                names.push((<any>node).declarations[0].id.name);
+                targets.push(path);
             }
         }
     });
+    return targets;
 
-    let ret:ParseResult = {fsmNames: names, content: generator(targets[0]).code};
-    return ret;
+    // let fsm0Code = generator(targets[0]).code;
+    // console.log(fsm0Code);
+
+
+
+    // let ret:ParseResult = {fsmNames: names, content: fsm0Code};
+    // return ret;
 }
+
 
--- view/libs/vizdraw.ts
+++ view/libs/vizdraw.ts
@@ -1,4 +1,9 @@
-"use strict";
+declare var callbacks:any;
+declare var targetFsm:any;
+declare var config:any;
+declare var Viz:any;
+declare var svgPanZoom:any;
+
 // default
 let curConfig = {
     rankdir: "TB",
@@ -11,282 +16,371 @@ let curConfig = {
     maxRowLetters: 20,
     style: 'rounded',
     weight: 1,
-};
-let fgColor = 'black';
-let gStates = new Map();
-let gEvents = new Map();
-function prepareFsmData(fsm) {
+}
+
+interface MyCallback {
+    key: string,
+    value: string
+}
+
+let fgColor = 'black'
+
+interface Info {
+    onenter?: string,
+    onentered?: string,
+    onleave?: string,
+    comments?: string | string[],
+    
+    shape?: string,
+    style?: string,
+}
+type InfoMap = Map<string,Info>;
+
+let gStates: InfoMap = new Map();
+let gEvents: InfoMap  = new Map();
+
+function prepareFsmData(fsm:any) {
     gStates.clear();
-    gEvents.clear();
+    gEvents.clear();    
+
+
     // set all event names, state names
-    if (fsm.events && fsm.events.length > 0) {
-        for (let i in fsm.events) {
-            let ev = fsm.events[i];
-            addToSet(ev.name, gEvents);
-            addToSet(ev.from, gStates);
-            addToSet(ev.to, gStates);
+    if(fsm.events && fsm.events.length > 0) {
+        for(let i in fsm.events) {
+            let ev = fsm.events[i];            
+            addToSet(ev.name, gEvents)
+            addToSet(ev.from, gStates)
+            addToSet(ev.to, gStates)          
         }
     }
-    if (fsm.initial) {
+
+    if(fsm.initial) {
         addToSet(fsm.inital, gStates);
     }
-    if (fsm.final) {
+
+    if(fsm.final) {
         addToSet(fsm.final, gStates);
     }
-    if (fsm.states) {
-        for (let i in fsm.states) {
+
+    if(fsm.states) {
+        for(let i in fsm.states) {
             let st = fsm.states[i];
-            if (st.name) {
+            if(st.name) {
                 addToSet(st.name, gStates);
             }
         }
     }
+    
     // deal with callbacks
-    if (callbacks && callbacks.length > 0) {
-        for (let i in callbacks) {
-            let cb = callbacks[i];
+    if(callbacks && callbacks.length > 0) {
+        for(let i in callbacks) {
+            let cb = callbacks[i] as MyCallback;
             handleCallback(cb);
         }
     }
+
     // deal with state defination
-    if (fsm.states && fsm.states.length > 0) {
-        for (let i in fsm.states) {
+    if(fsm.states && fsm.states.length > 0) {
+        for(let i in fsm.states) {
             let st = fsm.states[i];
             handleState(st);
         }
     }
 }
-function handleState(state) {
+
+function handleState(state:any) {
+    
     let stName = state.name;
-    if (gStates.has(stName)) {
-        for (let i in state) {
+    if(gStates.has(stName)) {
+        for(let i in state) {
             gStates.get(stName)[i] = state[i];
         }
     }
+    
 }
-function handleCallback(cb) {
+
+function handleCallback(cb: MyCallback) {
     let prefix = '';
     let remain = '';
+
     // prefix + state
     let allowedPrefix = ['onentered', 'onenter', 'onleave'];
-    for (let i in allowedPrefix) {
+    for(let i in allowedPrefix) {
         let ap = allowedPrefix[i];
-        if (cb.key.startsWith(ap)) {
+        if(cb.key.startsWith(ap)) {
             let rem = cb.key.substring(ap.length);
-            if (rem && gStates.has(rem)) {
+            if(rem && gStates.has(rem)) {
+               
                 gStates.get(rem)[ap] = cb.value;
                 return;
             }
         }
     }
 }
-function addToSet(input, container) {
-    if (!input)
+
+function addToSet(input:string | Array<string>, container: InfoMap) {
+    if(!input)
         return;
-    if (input.constructor === Array) {
-        input = input;
-        input.forEach((e) => {
-            if (!container.has(e))
+
+    if(input.constructor === Array) {
+        input = input as Array<string>;
+        input.forEach((e:any)=>{      
+            if(!container.has(e))      
                 container.set(e, {});
-        });
+        })
     }
     else {
-        input = input;
-        if (!container.has(input))
-            container.set(input, {});
+        input = input as string;
+        if(!container.has(input))      
+            container.set(input, {});        
     }
 }
+
 function applyConfig() {
-    if (!hasConfig())
+    if(!hasConfig()) 
         return;
+    
     applyObjectProperty(config, curConfig);
 }
-function applyObjectProperty(from, to) {
-    if (!from || !to)
+
+function applyObjectProperty(from:any, to:any) {
+    if(!from || !to)
         return;
-    for (let i in from) {
+
+    for(let i in from) {
         to[i] = from[i];
     }
 }
-function convertFsmToDot(fsm) {
+
+function convertFsmToDot(fsm:any) {
     prepareFsmData(fsm);
+
     applyConfig();
+    
     let head = getHead(fsm);
     let nodes = getNodes(fsm);
     let events = getEdges(fsm);
+
     let ret = `
     digraph finite_state_machine {
         ${head}
         ${nodes}
         ${events}
     }
-    `;
+    `
     console.log(ret);
-    return ret;
+	return ret;
 }
-function getHead(fsm) {
+
+function getHead(fsm:any) {
     let hasC = hasConfig();
     let rankdir = !hasC || !curConfig.rankdir ? 'TB' : config.rankdir;
-    let ret = `
+	let ret = `
 		rankdir=${rankdir};
 		bgcolor=transparent;
-	`;
-    return ret;
+	`
+	return ret;
 }
-function getEdges(fsm) {
+
+function getEdges(fsm:any) {
     let ret = `edge [fontname = ${curConfig.font}, color=${fgColor}]
-    `;
-    fsm.events.forEach((e) => {
+    `
+
+	fsm.events.forEach((e:any) => {
         let eventName = e.name;
-        ret += (e.from + ' -> ' + e.to + ` [ label="${'  ' + eventName}" ,weight=${curConfig.weight} ,arrowhead=${curConfig.arrowHead}, fontsize=${curConfig.fontSize}, color=${fgColor}, fontcolor=${fgColor} ];\n`);
-    });
-    return ret;
+		ret += (e.from + ' -> ' + e.to + ` [ label="${'  ' + eventName}" ,weight=${curConfig.weight} ,arrowhead=${curConfig.arrowHead}, fontsize=${curConfig.fontSize}, color=${fgColor}, fontcolor=${fgColor} ];\n`);
+	});
+	return ret;
 }
-function getNodes(fsm) {
+
+function getNodes(fsm:any) {
+    
     let ret = '';
+    
     // Initial & Final nodes
-    if (fsm.initial) {
+    if(fsm.initial) {
         ret += `
         ${fsm.initial} [shape = ${curConfig.initialShape}, fontname = ${curConfig.font}, fontsize=${curConfig.fontSize}, margin=0.15]; 
-        `;
-    }
-    if (fsm.final) {
+        `
+    }	 
+    if(fsm.final) {
         ret += `
         ${fsm.final} [shape = ${curConfig.finalShape}, fontname = ${curConfig.font}, fontsize=${curConfig.fontSize}, margin=0.15];
-        `;
+        `
     }
+    
     // General nodes, actually this part has no use now
     // every state has their separate attributes in getCommonNotes()
     ret += `
     node [shape = ${curConfig.nodeShape}, fontname = ${curConfig.font}, fontsize=${curConfig.fontSize}, margin=0.15];	    
-    `;
+    `
+    
     // Nodes with callback    
     ret += getCommonNotes(fsm);
     ret += '\n';
+
     // console.log(ret);
-    return ret;
+	return ret;
 }
-function isKindOf(target, cate) {
-    if (!cate)
+
+
+function isKindOf(target: string, cate:string | string[]) {
+  
+    if(!cate)
         return false;
-    if (cate.constructor === Array) {
-        cate = cate;
+
+    if(cate.constructor === Array) {
+        cate = cate as string[];
         return cate.includes(target);
     }
-    else
+    else 
         return target === cate;
 }
-function getCommonNotes(fsm) {
+
+function getCommonNotes(fsm:any) {
     let ret = "";
-    gStates.forEach((info, stName, map) => {
+    
+    gStates.forEach((info, stName, map) =>{
         let label;
-        if (info.onenter || info.onentered || info.onleave || info.comments) {
+        if(info.onenter || info.onentered || info.onleave || info.comments) {
             let title = stName;
             label = `<<table border="0" cellpadding="0" cellspacing="0"><tr><td>${title}</td></tr>`;
+
             // callbacks
             if (info.onenter || info.onentered || info.onleave)
                 label += getTableHrLine();
+
             if (info.onenter)
                 label += getTableItem("entry/" + info.onenter);
             if (info.onentered)
                 label += getTableItem("entry/" + info.onentered);
             if (info.onleave)
                 label += getTableItem("exit/" + info.onleave);
+
+
             // comments
             if (info.comments) {
                 label += getTableHrLine();
                 label += getCommentsTablePart(info.comments);
             }
-            label += `</table>>`;
+
+
+            label += `</table>>`;            
         }
+
         // label
-        let labelExp = label ? `,label=${label}` : ``;
+        let labelExp = label ? `,label=${label}` : ``;        
+
         // shape
         let shape = curConfig.nodeShape;
-        if (isKindOf(stName, fsm.initial))
+        if(isKindOf(stName, fsm.initial))
             shape = curConfig.initialShape;
-        else if (isKindOf(stName, fsm.final))
+        else if(isKindOf(stName, fsm.final))
             shape = curConfig.finalShape;
-        if (info.shape)
+        if(info.shape) 
             shape = info.shape;
+
         // style
         let style = curConfig.style;
-        if (info.style)
+        if(info.style) 
             style = info.style;
+
+
         let nodeItem = `
             ${stName} [shape = ${shape}, fontname = ${curConfig.font}, fontsize=${curConfig.fontSize}, margin=0.1, style=${style} ${labelExp}]
-            `;
+            `
         ret += nodeItem;
     });
     return ret;
 }
-function getCommentsTablePart(comments) {
+
+function getCommentsTablePart(comments:string | string[]) {
     let ret = '';
-    if (comments.constructor != Array) {
-        comments = comments;
+    if(comments.constructor != Array) {
+        comments = comments as string;
         let tempAr = [];
-        while (comments && comments.length > curConfig.maxRowLetters) {
+        while(comments && comments.length > curConfig.maxRowLetters) {
             let first = comments.substr(0, curConfig.maxRowLetters);
             tempAr.push(first);
-            comments = comments.substr(first.length);
+            comments = comments.substr(first.length);            
         }
         tempAr.push(comments);
         comments = tempAr;
     }
-    comments = comments;
-    comments.forEach(e => {
+    
+    comments = comments as string[];
+    comments.forEach(e=>{
         ret += getTableItem(e);
-    });
+    })
+
     return ret;
 }
+
 function getTableHrLine() {
     return `<tr><td cellpadding="2" cellspacing="2"></td></tr><hr/><tr><td cellpadding="2" cellspacing="2"></td></tr>`;
 }
-function getTableItem(content) {
+
+function getTableItem(content:string) {
     return `<tr><td align="left" cellpadding="0">${content}</td></tr>`;
 }
-function showErrorToUser(msg) {
+
+
+function showErrorToUser(msg:string) {
     $('#error-info').html('>_<<br/>' + msg);
 }
-function renderFsm(fsm) {
-    let findSvg = $('svg');
-    console.log(findSvg);
-    if (findSvg.length > 0) {
-        findSvg[0].remove();
-    }
-    let dot = '';
+
+
+function renderFsm(fsm:any) {
+
+    
+	let findSvg = $('svg');
+	console.log(findSvg);
+	if(findSvg.length > 0) {
+		findSvg[0].remove();
+	}
+
+
+    let dot = ''
     try {
-        dot = convertFsmToDot(fsm);
-    }
-    catch (error) {
+        dot = convertFsmToDot(fsm);    
+    } catch (error) {
         // The error message here is misleading
         // It will say 'inital' is not found when the fsm object is undefined by a missing external reference
         showErrorToUser("We found the fsm defination, but it can't be parsed !!" + error.message);
         return;
     }
+    
+
     let viz = new Viz();
     viz.renderSVGElement(dot)
-        .then(function (element) {
+    .then(function(element) {
+        
         document.body.appendChild(element);
-        panZoom = svgPanZoom(element, panConfig);
-        panZoom.zoom(0.8);
+		panZoom = svgPanZoom(element, panConfig)
+		panZoom.zoom(0.8);
     })
-        .catch(error => {
+    .catch(error => {
         // Create a new Viz instance (@see Caveats page for more info)
         viz = new Viz();
+
         // Possibly display the error
         console.error(error);
     });
 }
-let panConfig = {
+
+let panConfig = 
+{
     zoomEnabled: true,
     controlIconsEnabled: true,
     fit: true,
-    center: true,
-};
+    center: true,    
+ }
+
 let panZoom;
-$(document).ready(() => {
-    if (typeof targetFsm == "undefined") {
+
+$(document).ready(()=>{
+    
+    if(typeof targetFsm == "undefined") {        
         showErrorToUser(`We found the fsm defination, but it can't be parsed<br/>
 If you used <b>MemberExpression</b> in the FSM declaration, please use comments like this to wrap your code:<br/>
 <br/>
@@ -302,19 +396,21 @@ var fsm = {<br/>
 `);
     }
     else {
-        renderFsm(targetFsm);
+        renderFsm(targetFsm);	
     }
-});
+})
+
 function hasConfig() {
-    return typeof config != 'undefined';
+    return typeof config !=  'undefined';
 }
-$(window).resize(() => {
+
+$(window).resize(() =>{
     panZoom.resize();
     panZoom.center();
     panZoom.zoom(0.8);
-});
-window.onerror = function (message) {
+})
+
+window.onerror = function(message:any) {
     this.console.log(message);
-    this.console.log('name' + message.name);
-};
-//# sourceMappingURL=vizdraw.js.map
+    this.console.log('name' +　message.name);
+}
